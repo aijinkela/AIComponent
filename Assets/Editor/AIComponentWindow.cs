bb374350161aeec9910101afd764d95136ae4b99
x.y <gastrodia@outlook.com>
2023-05-19 03:04:11 +0800

    logo and fix miss add component bug

--- Assets/Editor/AIComponentWindow.cs
+++ Assets/Editor/AIComponentWindow.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
 using UnityEditor;
+using System;
+using System.IO;
 using System.Reflection;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Unity.EditorCoroutines.Editor;
 
 namespace AIComponent
 {
@@ -12,22 +18,45 @@ namespace AIComponent
       => "Write a Unity Compnent script.\n" +
          " - I only need the script body. Don’t add any explanation.\n" +
          " - You should add using statement at the top of the code to ensure that the code compilation will not report errors. \n" +
-         " - reply as the follow format .\n" +
-         "XXX(The Script Name)\n" +
-         "------------------- \n" +
-         "XXX(The Script Content)\n" +
          "The Component is described as follows:\n" + input;
 
         public void Generator()
         {
+            Debug.Log("prompt: " + _prompt);
             var warp = WrapPrompt(_prompt);
             var text = OpenAIUtil.InvokeChat(warp);
-            var splits = text.Split("-------------------");
-            var scriptName = splits[0].Replace("\n", "");
-            var scriptContent = splits[1].Replace("```", "").Replace("csharp", "");
+            Debug.Log("chatgpt resp: ");
+            Debug.Log(text);
+            var scriptName = GetClassName(text);
+            var scriptContent = text;
             var filename = "Assets/AIScripts/" + scriptName + ".cs";
             CreateScriptAsset(filename, scriptContent);
-            BindScriptAssetSync(filename);
+            // EditorCoroutineUtility.StartCoroutine(BindScriptAsset(filename, scriptName), this);  //some times not work 
+            BindScriptAsset(filename, scriptName);
+        }
+
+        private string GetClassName(string code) 
+        {
+            string className = null;
+            Regex regex = new Regex(@"(?:^|\s+)class\s+([a-zA-Z_][a-zA-Z0-9_]*)");
+            Match match = regex.Match(code);
+
+            if (match.Success)
+            {
+                className = match.Groups[1].Value;
+            }
+
+            return className;
+        }
+
+        public static Type GetAssemblyType(string typeName) {
+            var type = Type.GetType(typeName);
+            if (type != null) return type;
+            foreach (var a in AppDomain.CurrentDomain.GetAssemblies()) {
+                type = a.GetType(typeName);
+                if (type != null) return type;
+            }
+            return null;
         }
 
 
@@ -36,22 +65,39 @@ namespace AIComponent
             var flags = BindingFlags.Static | BindingFlags.NonPublic;
             var method = typeof(ProjectWindowUtil).GetMethod("CreateScriptAssetWithContent", flags);
             var script = method.Invoke(null, new object[] { file, code });
-            Debug.Log(script);
+
+            string fullPath = Path.GetFullPath(file);
+            File.WriteAllText(fullPath, code);
         }
 
-        private void BindScriptAssetSync(string file)
+        private MonoScript tmpScript;
+        private void Update() {
+            if(tmpScript != null){
+                Type classType = tmpScript.GetClass();
+                if (classType != null && classType.BaseType == typeof(MonoBehaviour))
+                {
+                    if(Selection.activeGameObject != null){
+                        Selection.activeGameObject.AddComponent(classType);
+                    }
+                    tmpScript = null;
+                }
+            }
+        }
+
+        private void BindScriptAsset(string file, string scriptName)
         {
-            AssetDatabase.ImportAsset(file);
+            AssetDatabase.ImportAsset(file, ImportAssetOptions.ForceSynchronousImport | ImportAssetOptions.ForceUpdate);
             var script = AssetDatabase.LoadAssetAtPath<MonoScript>(file);
-            Selection.activeGameObject.AddComponent(script.GetClass());
-            EditorUtility.SetDirty(Selection.activeGameObject);
+            if(Selection.activeGameObject != null){
+                tmpScript = script;
+            }
         }
 
         #endregion
 
         #region Editor GUI
 
-        string _prompt = "random rotate gameObject";
+        string _prompt = "random rotate";
 
         const string ApiKeyErrorText =
           "API Key hasn't been set. Please check the project settings " +
