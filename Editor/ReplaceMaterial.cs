2ff5a476ae8f9c750ce8f5418a02fd60fb94e84c
aijinkela <aijinkela@outlook.com>
2023-05-20 23:52:10 +0800

    添加AIComponent

---
+++ Editor/ReplaceMaterial.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEditor;
+
+public class ReplaceMaterial : AssetPostprocessor
+{
+    void OnPostprocessModel(GameObject g)
+    {
+        // Only process models imported from .blend files
+        if (!assetPath.ToLower().EndsWith(".blend")) return;
+
+        // Get a reference to the ShapeColorMat material
+        Material shapeColorMat = (Material)AssetDatabase.LoadAssetAtPath("Assets/AIBase/ShapeColorMat.mat", typeof(Material));
+
+        // Get all the mesh renderers in the imported model
+        MeshRenderer[] renderers = g.GetComponentsInChildren<MeshRenderer>();
+
+        foreach (MeshRenderer r in renderers)
+        {
+            // Check if the renderer's material is named "Vert"
+            if (r.sharedMaterial != null && r.sharedMaterial.name.Contains("Vert"))
+            {
+                // Assign the ShapeColorMat material to this renderer
+                r.sharedMaterial = shapeColorMat;
+            }
+        }
+
+        // Save the changes to the imported asset
+        // AssetDatabase.SaveAssets();
+    }
+}
